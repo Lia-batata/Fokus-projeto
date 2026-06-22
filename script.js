4d9481be0f0c58facc303d651353744b4bc7810f
Lia Batata <lialimafigueira@gmail.com>
2026-06-21 21:19:51 -0300

    Criando variáveis e botões de aplicação

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const tittle = document.querySelector('.app__title');
+
+const botaoIniciar = document.querySelector('.app__card-primary-button');
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
@@ -5,12 +11,19 @@ const longoBt = document.querySelector('.app__card-button--longo')
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
+
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900;
