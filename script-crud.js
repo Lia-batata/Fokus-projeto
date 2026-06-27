3a148150aebc9ea6d96927479e74461750481179
Lia Batata <lialimafigueira@gmail.com>
2026-06-26 23:38:06 -0300

    Submetendo formulário com localStorage

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,19 @@
 // encontrar o botão adicionar tarefa
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+            descricao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
 })
